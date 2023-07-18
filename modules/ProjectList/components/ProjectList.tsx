c39e9f7d3094241cf6ddcf2f42270e625e7b87c2
shaulov <shineginn@gmail.com>
2023-07-19 02:14:18 +0300

    Add styles for lg and sm breakpoints

--- modules/ProjectList/components/ProjectList.tsx
+++ modules/ProjectList/components/ProjectList.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import { styled } from "styled-components";
 import ProjectCard from "./ProjectCard";
+import { Devices } from "@/conts";
 
 function ProjectList() {
   return (
@@ -45,17 +45,57 @@ const List = styled.ul`
       content: "";
       position: absolute;
       top: 0;
-      right: -21px;
+      right: -25px;
       height: 100%;
       border-right: 1px solid #242628;
     }
     &:after{
       content: "";
       position: absolute;
-      bottom: -21px;
+      bottom: -25px;
       left: 0;
       width: 100%;
       border-bottom: 1px solid #242628;
+    }
+  }
+  @media screen and ${Devices.LG} {
+    grid-template-columns: repeat(2, 1fr);
+    .projects-item {
+      position: relative;
+      &:nth-child(n){
+        &:before, &:after{
+          display: inherit;
+        }
+      }
+      &:nth-child(2n){
+        &:before{
+          display: none;
+        }
+      }
+      &:nth-last-child(-n+2) {
+        &:after{
+          display: none;
+        }
+      }
+    }
+  }
+  @media screen and ${Devices.SM} {
+    grid-template-columns: 1fr;
+    .projects-item {
+      position: relative;
+      &:nth-child(n){
+        &:after {
+          display: inherit;
+        }
+        &:before {
+          display: none;
+        }
+      }
+      &:last-child {
+        &:after {
+          display: none;
+        }
+      }
     }
   }
 `;
